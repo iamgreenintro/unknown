8f6029b3de166eb0b8916f626b51aa8cde319d64
iamgreenintro <iamgreenintro@outlook.com>
2025-07-08 23:35:47 +0200

    form wip started

--- apps/app-administrator/src/app/components/login/login.ts
+++ apps/app-administrator/src/app/components/login/login.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  public showLoginForm: boolean = true;
+
+  // Login Form:
+  public username = new FormControl('', [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
+  public password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
+  constructor() {
+    //
+  }
+
+  public login() {
+    console.log(this.username.value, this.password.value);
+  }
+
+  public registerNewUser() {
+    console.log(this.username.value, this.password.value);
+  }
+}
